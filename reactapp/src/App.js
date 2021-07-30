674c1e0cfec0be005c8e1b0fc59ea987fef86e82
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2021-07-30 21:24:47 +0530

    Mounted volumes in docker compose

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nice start
+          Nice start !!
         </p>
       </header>
     </div>
